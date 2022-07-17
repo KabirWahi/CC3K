47bd64502f51a0929f7d29de09c9f1544510da42
AliceKan <33497064+AliceKan@users.noreply.github.com>
2022-07-17 17:29:52 -0400

    Update potion.cc

--- potion.cc
+++ potion.cc
@@ -4,14 +4,22 @@
 #include <string>
 using namespace std;
 
-Potion::Potion(Posn position, string name): Item{'P', position}, name{name} {
+Potion::Potion(Posn position, string name): Item{' ', position}, name{name} {
     if (name == "RH") {
         value = 10;
+        symbol = '0';
     } else if (name == "PH") {
         value = -10;
-    } else {
-        value = 0;
-    }
+        symbol = '3';
+    } else if (name == "BA") {
+        symbol = '1';
+    } else if (name == "BD") {
+        symbol = '2';
+    } else if (name == "WA") {
+        symbol = '4';
+    } else if (name == "WD") {
+        symbol = '5';
+    } else {} 
 }
 
 Potion::~Potion() {}
