6543c726608aeb572208eb2b1836feebbd5133a8
AliceKan <33497064+AliceKan@users.noreply.github.com>
2022-07-17 17:30:54 -0400

    Update gold.cc

--- gold.cc
+++ gold.cc
@@ -4,16 +4,17 @@
 #include <string>
 using namespace std;
 
-Gold::Gold(Posn position, string name): Item{'G', position}, name{name} {
+Gold::Gold(Posn position, string name): Item{' ', position}, name{name} {
     if (name == "Normal") {
         value = 1;
+        symbol = 6;
     } else if (name == "SmallHoard") {
         value = 2;
+        symbol = 7;
     } else if (name == "MerchantHoard") {
         value = 4;
-    } else {
-        value = 0;
-    }
+        symbol = 8;
+    } else {}
 }
 
 Gold::~Gold() {}
