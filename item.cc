a74445f9f9a193740954a61b514d180117e57bb6
AliceKan <33497064+AliceKan@users.noreply.github.com>
2022-07-17 14:08:01 -0400

    Create item.cc

---
+++ item.cc
@@ -0,0 +1,26 @@
+#include "item.h"
+#include "posn.h"
+
+Item::Item(char symbol, Posn position): symbol{symbol}, position{position}, value{0}, guarded{false} {}
+
+Item::~Item() {}
+
+char Item::getSymbol() {
+    return symbol;
+}
+    
+Posn Item::getPosition() {
+    return position;
+}
+    
+int Item::getValue() {
+    return value;
+}
+    
+bool Item::isGuarded() {
+    return false;
+}
+
+void Item::setGuarded() {
+    return;
+}
