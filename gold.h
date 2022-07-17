9722606d13fd08530930c79f86c73c01b40cbe19
AliceKan <33497064+AliceKan@users.noreply.github.com>
2022-07-17 14:10:33 -0400

    Create gold.h

---
+++ gold.h
@@ -0,0 +1,14 @@
+#ifndef GOLD_H
+#define GOLD_H
+#include <string>
+#include "item.h"
+
+class Gold: public Item {
+    protected:
+    std::string name;
+    public:
+    Gold(char symbol, Posn position, std::string name, int value);
+    ~Gold();
+};
+
+#endif
