1abdecbf407353b77f06023bb39b0de9ce165ad4
AliceKan <33497064+AliceKan@users.noreply.github.com>
2022-07-17 14:43:17 -0400

    Update potion.cc

--- potion.cc
+++ potion.cc
@@ -4,8 +4,14 @@
 #include <string>
 using namespace std;
 
-Potion::Potion(char symbol, Posn position, string name, int value): Item{symbol, position}, name{name} {
-    this->value = value;
+Potion::Potion(Posn position, string name): Item{'P', position}, name{name} {
+    if (name == "RH") {
+        value = 10;
+    } else if (name == "PH") {
+        value = -10;
+    } else {
+        value = 0;
+    }
 }
 
 Potion::~Potion() {}
