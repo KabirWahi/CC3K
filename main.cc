7d517ea91de8fa853336506da01e8753d524d27d
KabirWahi <kabirwahi@gmail.com>
2022-07-21 14:47:11 -0400

    ending, restart and start

--- main.cc
+++ main.cc
@@ -4,7 +4,18 @@
 using namespace std;
 
 int main() {
-    Game game('h');
+    cout << "Welcome to CC3K!" << endl;
+    cout << "Please choose your character:" << endl;
+    cout << "Human (h)" << endl;
+    cout << "Dwarf (d)" << endl;
+    cout << "Elf (e)" << endl;
+    cout << "Orc (o)" << endl;
+    char playerSymbol;
+    cin >> playerSymbol;
+    while (playerSymbol != 'h' && playerSymbol != 'd' && playerSymbol != 'e' && playerSymbol != 'o') {
+        cout << "Invalid character. Please try again." << endl;
+        cin >> playerSymbol;
+    }
+    Game game(playerSymbol);
     game.play();
-    return 0;
 }
