56f211e6fd91411d70141c4ffe57a186bcde080f
kanhaichun <kanhaichun@gmail.com>
2022-07-21 14:22:00 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-int main() {
-    Game game('h');
+int main(int argc, char *argv[]) {
+    char r = 'h';
+    if (argc != 1 && (*argv[1] == 'e' || *argv[1] == 'h' || *argv[1] == 'd' || *argv[1] == 'o')) {
+        r = *argv[1];
+    }
+    Game game(r);
     game.play();
     return 0;
 }
