8b1533f1914c52287bec5f72d33cbb7a85f77bd5
KabirWahi <kabirwahi@gmail.com>
2022-07-21 19:44:13 -0400

    lmao

--- dwarf.cc
+++ dwarf.cc
@@ -3,12 +3,6 @@
 using namespace std;
 
 Dwarf::Dwarf(Posn p) {
-  knownPotions[0] = false;
-  knownPotions[1] = false;
-  knownPotions[2] = false;
-  knownPotions[3] = false;
-  knownPotions[4] = false;
-  knownPotions[5] = false;
   atk = 20;
   def = 20;
   HP = maxHP;
